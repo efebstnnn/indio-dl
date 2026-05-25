78d3bb6ec5302350dcb7db2d1e04b184434689fe
efe <266557548+efebstnnn@users.noreply.github.com>
2026-05-25 23:23:14 +0300

    exe dosyası oluşturuldu, dosyanın içinde gerekli paketler hazır gelir

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import os
+import sys
+
 import fonts
 from gui import DownloaderApp
 from theme import Tema
+
+if getattr(sys, 'frozen', False):
+    BASE_DIR = sys._MEIPASS
+else:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+os.environ["PATH"] = BASE_DIR + os.pathsep + os.environ.get("PATH", "")
 
 if __name__ == "__main__":
     fonts.yukle()
